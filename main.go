812cda5d9ed1354db961104d9e6d318a9875f5a4
bokyun91 <braun@todayhenry.co>
2020-12-16 21:44:59 +0900

    eclare variable in GO

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+// 대문자로 시작할 경우(함수, 변수) -> public
 func main() {
-	fmt.Println("Hello world")
+	// 상수 const, 변수 var
+	// 변수 선언 => var/const + 변수명 + 자료형
+	var name string = "nico"
+	name = "lyn"
+	fmt.Println(name)
 }
