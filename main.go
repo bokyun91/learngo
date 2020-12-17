8ce110f8a6dd79936ab629c5235f5a89f7151b8d
bokyun91 <braun@todayhenry.co>
2020-12-17 19:13:51 +0900

    Functions part One

--- main.go
+++ main.go
@@ -1,6 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+// 자바와 다르게 변수명 뒤에 type 작성. return 타입은 arg 뒤에 작성
+func multiply(a int, b int) int {
+	return a * b
+}
+
+// 여러 개 반환 가능
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+// 같은 타입의 arg를 여러 개 받을 경우 ...으로 받을 수 있음
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
 
 // 대문자로 시작할 경우(함수, 변수) -> public
 func main() {
@@ -9,4 +27,13 @@ func main() {
 	var name string = "nico"
 	name = "lyn"
 	fmt.Println(name)
+	fmt.Println(multiply(2, 2))
+
+	// 여러 변수 return 받을 때. 하나만 사용할 경우 _ .
+	totalLength, upperName := lenAndUpper("nico")
+	totalLength2, _ := lenAndUpper("pbk")
+	fmt.Println(totalLength, upperName)
+	fmt.Println(totalLength2)
+
+	repeatMe("nico", "lynn", "dal", "marl")
 }
